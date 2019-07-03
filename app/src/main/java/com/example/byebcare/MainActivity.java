3095150d0dbab988443c6b3fe6ba89740d1653fe
Geonhyeong Lim <geonhyeong.lim@icloud.com>
2019-07-03 13:01:53 +0530

    Update MainActivity.java Fixed Problems with notification in different versions

--- app/src/main/java/com/example/byebcare/MainActivity.java
+++ app/src/main/java/com/example/byebcare/MainActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 import butterknife.BindView;
@@ -22,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
 
     private JSONObject jsonObject;
     private Boolean babyIsInDanger = false;
+
+    String channelId = "CHANNEL_NO_1";
     @BindView(R.id.button) Button button;
     @BindView(R.id.textView) TextView textView;
 
@@ -50,15 +54,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void sendNotification() {
-
+        String CHANNEL_ID = createNotificationChannel();
 
         NotificationCompat.Builder builder =
-                new NotificationCompat.Builder(this, null)
+                new NotificationCompat.Builder(this, CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_launcher_background)
                 .setContentTitle("dasdf")
-                .setContentText("asdfasdfasfd")
-                .setDefaults(Notification.DEFAULT_ALL)
+                .setContentText("asdfasdfasfd");
+
+        if(CHANNEL_ID == null) {
+            builder.setDefaults(Notification.DEFAULT_ALL)
                 .setPriority(Notification.PRIORITY_MAX);
+        } else {
+            builder.setDefaults(Notification.DEFAULT_ALL)
+                    .setPriority(NotificationManager.IMPORTANCE_HIGH)
+                    .setChannelId(channelId);
+        }
+
 
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://10.4.104.131"));
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
@@ -67,6 +79,41 @@ public class MainActivity extends AppCompatActivity {
 
         NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(001, builder.build());
+    }
+
+    private String createNotificationChannel() {
+        // NotificationChannels are required for Notifications on O (API 26) and above.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            // The id of the channel.
+
+            // The user-visible name of the channel.
+            CharSequence channelName = "N";
+            // The user-visible description of the channel.
+            String channelDescription = "d";
+            int channelImportance = NotificationManager.IMPORTANCE_HIGH;
+            boolean channelEnableVibrate = true;
+            int channelLockscreenVisibility = NotificationCompat.VISIBILITY_PRIVATE;
+
+            // Initializes NotificationChannel.
+            NotificationChannel notificationChannel =
+                    new NotificationChannel(channelId, channelName, channelImportance);
+            notificationChannel.setDescription(channelDescription);
+            notificationChannel.enableVibration(channelEnableVibrate);
+            notificationChannel.setLockscreenVisibility(channelLockscreenVisibility);
+
+            // Adds NotificationChannel to system. Attempting to create an existing notification
+            // channel with its original values performs no operation, so it's safe to perform the
+            // below sequence.
+            NotificationManager notificationManager =
+                    (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(notificationChannel);
+
+            return channelId;
+        } else {
+            // Returns null for pre-O (26) devices.
+            return null;
+        }
     }
 
 
