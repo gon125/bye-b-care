96bb29e3de453690c5442ff5484fc326974ba166
seungyi <ssoyi34@naver.com>
2019-07-05 20:40:06 +0900

    update GraphActivity.java
    line and column graph combine work.
    line graph with temperature and column graph with BPM.
    i make two y-axis and give some measure.

--- app/src/main/java/com/example/byebcare/GraphActivity.java
+++ app/src/main/java/com/example/byebcare/GraphActivity.java
@@ -4,19 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+// org.json.JSONException;
+//import org.json.JSONObject;
 
 import com.anychart.AnyChart;
 import com.anychart.AnyChartView;
 import com.anychart.chart.common.dataentry.DataEntry;
 import com.anychart.chart.common.dataentry.ValueDataEntry;
 import com.anychart.charts.Cartesian;
+import com.anychart.core.cartesian.series.Column;
 import com.anychart.core.cartesian.series.Line;
 import com.anychart.data.Mapping;
 import com.anychart.data.Set;
 import com.anychart.enums.Anchor;
 import com.anychart.enums.MarkerType;
+import com.anychart.enums.Orientation;
+import com.anychart.enums.Position;
 import com.anychart.enums.TooltipPositionMode;
 import com.anychart.graphics.vector.Stroke;
+import com.anychart.scales.Linear;
+import com.anychart.core.cartesian.series.Base;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +31,18 @@ import java.util.List;
 
 public class GraphActivity extends AppCompatActivity {
 
+   /* private JSONObject jsonObject;
+
+   public GraphActivity(JSONObject jsonObject){
+        this.jsonObject = jsonObject;
+        try{
+            Number tempAmb = (Number)jsonObject.get("A");
+            Number tempB = (Number)jsonObject.get("O");
+            Number bpm = (Number) jsonObject.get("B");
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+    }   */
     @BindView(R.id.any_chart_view) AnyChartView anyChartView;
 
     @Override
@@ -34,10 +53,11 @@ public class GraphActivity extends AppCompatActivity {
         anyChartView.setProgressBar(findViewById(R.id.progress_bar));
 
         Cartesian cartesian = AnyChart.line();
+        Cartesian cartesian2 = AnyChart.column();
 
         cartesian.animation(true);
 
-        cartesian.padding(10d, 20d, 5d, 20d);
+        cartesian.padding(5d, 10d, 5d, 10d);
 
         cartesian.crosshair().enabled(true);
         cartesian.crosshair()
@@ -46,83 +66,82 @@ public class GraphActivity extends AppCompatActivity {
                 .yStroke((Stroke) null, null, null, (String) null, (String) null);
 
         cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+        cartesian.title("Record of your baby condition");
+        cartesian.yAxis(0).title("Temperature(celsius)");
 
-        cartesian.title("Trend of Sales of the Most Popular Products of ACME Corp.");
+        Linear scalesLinear2 = Linear.instantiate();
+        scalesLinear2.minimum(40d);
+        scalesLinear2.maximum(100d);
+        scalesLinear2.ticks("{interval:10}");
+        
 
-        cartesian.yAxis(0).title("Number of Bottles Sold (thousands)");
+        com.anychart.core.axes.Linear extraYAxis = cartesian.yAxis(1);
+        cartesian.yAxis(1).title("Pulse(number/minute)");
+        extraYAxis.orientation(Orientation.RIGHT)
+                .scale(scalesLinear2);
+        extraYAxis.labels()
+                .padding(0d,0d,0d,5d);
+
+        
         cartesian.xAxis(0).labels().padding(5d, 5d, 5d, 5d);
 
         List<DataEntry> seriesData = new ArrayList<>();
-        seriesData.add(new CustomDataEntry("1986", 3.6, 2.3, 2.8));
-        seriesData.add(new CustomDataEntry("1987", 7.1, 4.0, 4.1));
-        seriesData.add(new CustomDataEntry("1988", 8.5, 6.2, 5.1));
-        seriesData.add(new CustomDataEntry("1989", 9.2, 11.8, 6.5));
-        seriesData.add(new CustomDataEntry("1990", 10.1, 13.0, 12.5));
-        seriesData.add(new CustomDataEntry("1991", 11.6, 13.9, 18.0));
-        seriesData.add(new CustomDataEntry("1992", 16.4, 18.0, 21.0));
-        seriesData.add(new CustomDataEntry("1993", 18.0, 23.3, 20.3));
-        seriesData.add(new CustomDataEntry("1994", 13.2, 24.7, 19.2));
-        seriesData.add(new CustomDataEntry("1995", 12.0, 18.0, 14.4));
-        seriesData.add(new CustomDataEntry("1996", 3.2, 15.1, 9.2));
-        seriesData.add(new CustomDataEntry("1997", 4.1, 11.3, 5.9));
-        seriesData.add(new CustomDataEntry("1998", 6.3, 14.2, 5.2));
-        seriesData.add(new CustomDataEntry("1999", 9.4, 13.7, 4.7));
-        seriesData.add(new CustomDataEntry("2000", 11.5, 9.9, 4.2));
-        seriesData.add(new CustomDataEntry("2001", 13.5, 12.1, 1.2));
-        seriesData.add(new CustomDataEntry("2002", 14.8, 13.5, 5.4));
-        seriesData.add(new CustomDataEntry("2003", 16.6, 15.1, 6.3));
-        seriesData.add(new CustomDataEntry("2004", 18.1, 17.9, 8.9));
-        seriesData.add(new CustomDataEntry("2005", 17.0, 18.9, 10.1));
-        seriesData.add(new CustomDataEntry("2006", 16.6, 20.3, 11.5));
-        seriesData.add(new CustomDataEntry("2007", 14.1, 20.7, 12.2));
-        seriesData.add(new CustomDataEntry("2008", 15.7, 21.6, 10));
-        seriesData.add(new CustomDataEntry("2009", 12.0, 22.5, 8.9));
+        seriesData.add(new CustomDataEntry("01", 22.5, 33.3, 65));
+        seriesData.add(new CustomDataEntry("02", 25.5, 37.3, 77));
+        seriesData.add(new CustomDataEntry("03", 26.5, 32.5, 80));
+        seriesData.add(new CustomDataEntry("04", 18.5, 28.3, 88));
+        seriesData.add(new CustomDataEntry("05", 28.5, 25.7, 50));
+
+
 
         Set set = Set.instantiate();
         set.data(seriesData);
-        Mapping series1Mapping = set.mapAs("{ x: 'x', value: 'value' }");
-        Mapping series2Mapping = set.mapAs("{ x: 'x', value: 'value2' }");
-        Mapping series3Mapping = set.mapAs("{ x: 'x', value: 'value3' }");
+        Mapping tempAmbData = set.mapAs("{ x: 'x', value: 'tempAmb' }");
+        Mapping tempBData = set.mapAs("{ x: 'x', value: 'tempB' }");
+        Mapping PulseData = set.mapAs("{ x: 'x', value: 'bpm' }" );
 
-        Line series1 = cartesian.line(series1Mapping);
-        series1.name("Brandy");
-        series1.hovered().markers().enabled(true);
-        series1.hovered().markers()
+
+        Line seriesA = cartesian.line(tempAmbData);
+        seriesA.name("Ambient.t");
+        seriesA.hovered().markers().enabled(true);
+        seriesA.hovered().markers()
                 .type(MarkerType.CIRCLE)
-                .size(4d);
-        series1.tooltip()
+                .size(5d);
+        seriesA.tooltip()
                 .position("right")
                 .anchor(Anchor.LEFT_CENTER)
                 .offsetX(5d)
                 .offsetY(5d);
 
-        Line series2 = cartesian.line(series2Mapping);
-        series2.name("Whiskey");
-        series2.hovered().markers().enabled(true);
-        series2.hovered().markers()
+        Line seriesB = cartesian.line(tempBData);
+        seriesB.name("Baby.t");
+        seriesB.hovered().markers().enabled(true);
+        seriesB.hovered().markers()
                 .type(MarkerType.CIRCLE)
-                .size(4d);
-        series2.tooltip()
-                .position("right")
-                .anchor(Anchor.LEFT_CENTER)
+                .size(5d);
+        seriesB.tooltip()
+                .position("left")
+                .anchor(Anchor.CENTER)
                 .offsetX(5d)
                 .offsetY(5d);
 
-        Line series3 = cartesian.line(series3Mapping);
-        series3.name("Tequila");
-        series3.hovered().markers().enabled(true);
-        series3.hovered().markers()
+        Column seriesP = cartesian.column(PulseData);
+        seriesP.yScale(scalesLinear2);
+        seriesP.name("BPM");
+        seriesP.hovered().markers().enabled(true);
+        seriesP.hovered().markers()
                 .type(MarkerType.CIRCLE)
-                .size(4d);
-        series3.tooltip()
-                .position("right")
-                .anchor(Anchor.LEFT_CENTER)
-                .offsetX(5d)
-                .offsetY(5d);
+                .size(5d);
+        seriesP.tooltip()
+                .position(Position.CENTER_BOTTOM)
+                .anchor(Anchor.CENTER_BOTTOM)
+                .offsetX(0d)
+                .offsetY(5d)
+                .format("${%Value}{groupsSeparator:}");
 
         cartesian.legend().enabled(true);
-        cartesian.legend().fontSize(13d);
-        cartesian.legend().padding(0d, 0d, 10d, 0d);
+        cartesian.legend().fontSize(15d);
+        cartesian.legend().padding(0d, 0d, 15d, 0d);
 
         anyChartView.setChart(cartesian);
 
@@ -130,10 +149,10 @@ public class GraphActivity extends AppCompatActivity {
 
     private class CustomDataEntry extends ValueDataEntry {
 
-        CustomDataEntry(String x, Number value, Number value2, Number value3) {
-            super(x, value);
-            setValue("value2", value2);
-            setValue("value3", value3);
+        CustomDataEntry(String x,Number tempAmb, Number tempB, Number bpm) {
+            super(x, tempAmb);
+            setValue("tempB", tempB);
+            setValue("bpm", bpm);
         }
 
     }
